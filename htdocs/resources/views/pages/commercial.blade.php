ccf2b70e18f3f3c6215cc91a220b64c55f6ac154
Chris Sewell <chris@touson.co.uk>
2017-06-26 21:33:07 +0100

    Updated content in commercials page

--- htdocs/resources/views/pages/commercial.blade.php
+++ htdocs/resources/views/pages/commercial.blade.php
@@ -9,38 +9,103 @@
 <section>
 	<div class="container">
 		<div class="col-set">
-			<div class="col-2">
-				<h4>RSF GROUP has a dedicated accounts team to deal with all enquiries</h4>
-				<p>We can offer account facilities to domestic and business customers that use us on a regular basis over a period of years, and meet with our strict credit criteria.</p>
+			<div class="col-1">
+				<h1>Building Services</h1>
 			</div>
 			<div class="col-2">
-				<p>Our straight forward accounting procedure provides a convenient, easy and quick way to pay for your plumbing and maintenance services.</p>
-				<p>If your application for a credit account is successful, you will be informed in writing and given an account number. Account facilities are subject to terms and conditions.</p>
+				<h3>Electrical Services</h3>
+				<p>Safety is our top priority, which is why all our electrical work is carried to current wiring regulations and ensures you are complying with the IEE codes of practice.</p>
+				<ul class="tick">
+					<li>Socket &amp; lighting installations</li>
+					<li>Smoke alarms</li>
+					<li>Office &amp; shop re-lamping &amp; rewiring</li>
+					<li>Full commercial electrical system certification</li>
+					<li>Emergency lighting</li>
+					<li>Distributed boards &amp; consumer lighting</li>
+					<li>Commercial installation repairs</li>
+					<li>Commercial new build wiring &amp; rewiring</li>
+					<li>Commercial lighting &amp; security lighting</li>
+					<li>Commercial single phase &amp; 3 phase systems</li>
+					<li>Earth bounding</li>
+					<li>Landlord inspections and safety certification</li>
+					<li>Insurance reports</li>
+				</ul>
+			</div>
+			<div class="col-2">
+				<h3>Air Conditioning Maintenance</h3>
+				<p>We can help you to provide a comfortable working environment with the implementation of commercial air conditioning. This reduces the need to open windows which can cause noise pollution and keep the temperature at a controlled rate. RSF Group not only supplies air conditioning systems but can also maintain your current one to ensure you are using the most cost effective and energy efficient system. Services we offer include:</p>
+				<ul class="tick">
+					<li>Air conditioning and maintenance services</li>
+					<li>Environmental control services</li>
+					<li>Ventilation services</li>
+					<li>Refrigeration and control cooling</li>
+					<li>24 hour emergency response service</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-set">
+			<div class="col-2">
+				<h3>Commercial Gas</h3>
+				<p>Ensuring your boiler plant is well maintained not only saves you money but reduces the risk of a failure. The RSF Group can provide a full range of boiler services from maintenance to supplying a new boiler plant system. Our team of HVCA qualified engineers are also experienced to ensure work is carried out to safety standards and completed efficiently in all types of commercial premises. This includes helping landlords to ensure their gas safety is to CP12 requirements. Whatever your requirements we can help. Our services include:</p>
+				<ul class="tick">
+					<li>Boiler plant maintenance</li>
+					<li>Boiler combustion</li>
+					<li>Hot water plant maintenance services</li>
+					<li>Gas safety inspections</li>
+					<li>Heating system repairs</li>
+				</ul>
+			</div>
+			<div class="col-2">
+				<h3>Plumbing</h3>
+				<p>From a leaky tap to a newly fitted system, we can help ensure your water system is running an effectively and reliably service. RSF’s team of experienced and qualified plumbers can assist you with all your needs. Our services include:</p>
+				<ul class="tick">
+					<li>Burst pipes &amp; leaky tap repairs</li>
+					<li>Installation of power showers</li>
+					<li>Washing machine installation</li>
+					<li>Dish washer installation</li>
+					<li>Gutters</li>
+					<li>Flood restorations</li>
+					<li>Cylinders</li>
+					<li>Immersion heaters</li>
+					<li>Shower pumps</li>
+					<li>Storage tanks</li>
+					<li>Bell valves</li>
+					<li>All insurance work carried out </li>
+				</ul>
 			</div>
 		</div>
 		<div class="col-set">
 			<div class="col-1">
-				<h2>Terms and Conditions</h2>
+				<h1>Commercial Gas</h1>
+			</div>
+			<div class="col-3">
+				<h3>Keep safe with our qualified engineers</h3>
+				<p>Reduce your fuel costs by ensuring your system is working to its optimum. The RSF Group can implement and maintain your boiler plant to ensure it is reliable and efficient. All with the reassurance that all our engineers are HVCA approved, and work within Health and Safety standards who are experienced on commercial premises.</p>
+				<p>We’re also Gas Safety Registered for both gas and oil powered plants.</p>
+				<p>With our latest equipment which enables our engineers to fine tune your system to meet current emission and efficiency standards, you’ll also have the reassurance you’re working within the law.</p>
+			</div>
+			<div class="col-3">
+				<h3>All types of work, supplied on any premises</h3>
+				<p>Our services range from whether you need a new pipe work system or simply a single appliance installed. Either way, you can expect a full explanation from our engineers so you are kept informed of the work which is required, and carried out.</p>
+				<p>We provide for shops, offices, industrial units, schools, local authorities, restaurants and many more.</p>
+				<p>Our work doesn’t only include maintenance and supply, but we can also carry out examinations of your current heating controls to give to recommendations on how your boiler can run more efficiently, and cost effective.</p>
+			</div>
+			<div class="col-3">
+				<h3>Our services</h3>
+				<p>Whatever the problem - we can help quickly and effeciently. Our services include:</p>
 				<ul class="tick">
-					<li>A strict £5,000 credit limit is available.</li>
-					<li>Strictly 30 days credit available from date of invoice.</li>
-					<li>Monthly itemised invoicing.</li>
-					<li>Credit facilities will only be granted to those that have used Pimlico on a regular basis, over a long period of time.</li>
-					<li>Credit checks will be carried out and acceptance is subject to receipt of satisfactory references and approval.</li>
-					<li>Exceeding your account limit, or missing a payment, will result in the account being suspended until payment is received.</li>
-					<li>Personal information is held in strict confidence and not passed onto other organisations except for credit purposes.</li>
-					<li>Pimlico reserves the right to withdraw credit facilities if the account becomes overdue or the monthly credit limit is exceeded.</li>
+					<li>Gas safety inspections</li>
+					<li>Hot water plant maintenance</li>
+					<li>Boiler plant maintenance </li>
+					<li>Boiler combustion</li>
+					<li>Heating system repairs</li>
+					<li>Pipe work installation</li>
+					<li>Safety and control systems</li>
+					<li>Burner installation, maintenance &amp; repair</li>
+					<li>Process plant and machinery</li>
+					<li>Overhead air heater</li>
 				</ul>
-			</div>
-			<div class="col-1">
-				<h2>Applications</h2>
-			</div>
-			<div class="col-2">
-				<p>To apply for credit account please write to our Accounts Manager:</p>
-				<p>Mr Andy White RSF Group .9 – 11 Butts Green Rd Hornchurch Essex RM11 2JR</p>
-			</div>
-			<div class="col-2">
-				@include('partials.telephone-block')
+				<p>Specialists in Ambirad Servicing, Keston Servicing, Powrmatic Servicing</p>
 			</div>
 		</div>
 	</div>
