95505d02bc02c64f9cd7e7411162ea51e7560950
Chris Sewell <chris@touson.co.uk>
2017-07-04 21:34:53 +0100

    Rejigged header placement

--- htdocs/resources/views/include/header.blade.php
+++ htdocs/resources/views/include/header.blade.php
@@ -6,16 +6,19 @@
 			</a>
 		</div>
 		<div class="contact-info">
-			<div class="tel">01708 444 355</div>
 			<div class="email">
 				<a href="mailto:info@rsfgroup.co.uk">info@rsfgroup.co.uk</a>
+			</div>
+			<div class="tel">
+				<span class="message">need an engineer?</span>
+				<span>01708 444 355</span>
 			</div>
 		</div>
 		<div class="quote-button">
 			<a href="/contact-us" class="quick-quote">Quick Quote</a>
 		</div>
-		<p id="strapline">London and Essex premier heating and plumbing company</p>
 	</div>
+	<p id="strapline">London and Essex premier heating and plumbing company</p>
 </header>
 
 @include('include.nav')
